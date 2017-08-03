2a97a80e5b982e764e43442483f2b52b874d5bdb
Sergej Alikov <sergej.alikov@gmail.com>
2017-08-03 22:16:19 +0100

    Implement TLSTransport
    This version of the TLSTransport supports both client key/cert
    and server certificate checking

---
+++ riemann_fping/transport.py
@@ -0,0 +1,24 @@
+import ssl
+
+from riemann_client.transport import TCPTransport, HOST, PORT, TIMEOUT
+
+
+class TLSTransport(TCPTransport):
+    def __init__(
+            self, host=HOST, port=PORT, timeout=TIMEOUT,
+            keyfile=None, certfile=None, ca_certs=None):
+
+        super(TLSTransport, self).__init__(host, port, timeout)
+        self.keyfile = None
+        self.certfile = None
+        self.ca_certs = ca_certs
+
+    def connect(self):
+        super(TLSTransport, self).connect()
+        self.socket = ssl.wrap_socket(
+            self.socket,
+            keyfile=self.keyfile,
+            certfile=self.certfile,
+            ssl_version=ssl.PROTOCOL_TLSv1,
+            cert_reqs=ssl.CERT_REQUIRED,
+            ca_certs=self.ca_certs)
