aa122870e6b635810cfd94be84be0024779f0b8e
Sergej Alikov <sergej.alikov@gmail.com>
2017-08-03 17:43:42 +0100

    Add Fping class with summary parser

---
+++ riemann_fping/fping.py
@@ -0,0 +1,45 @@
+# This file is part of riemann-fping.
+
+# Automated is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+
+RE_HOST = r'(?P<host>.*)'
+RE_XMT_RCV_LOSS = r'(?P<xmt>[0-9]+)\/(?P<rcv>[0-9]+)\/(?P<loss>[0-9.]+)%'
+RE_MIN_AVG_MAX = r'(?P<min>[0-9.]+)\/(?P<avg>[0-9.]+)\/(?P<max>[0-9.]+)'
+
+RE_FPING_SUMMARY = r'^{} : xmt\/rcv\/%loss = {}, min\/avg\/max = {}$'.format(
+    RE_HOST, RE_XMT_RCV_LOSS, RE_MIN_AVG_MAX)
+
+
+class Fping:
+    def __init__(self, ttl=60):
+        self.ttl = ttl
+
+    def parse(self, fping_output):
+
+        lines = fping_output.splitlines()
+        re_summary = re.compile(RE_FPING_SUMMARY)
+
+        for line in lines:
+
+            match = re_summary.match(line)
+
+            if match is not None:
+                for measurement in ('min', 'avg', 'max', 'loss'):
+                    yield {
+                        'service': 'fping/{}'.format(measurement),
+                        'host': match.group('host'),
+                        'metric': float(match.group(measurement)),
+                    }
---
+++ riemann_fping/tests/test_fping.py
@@ -0,0 +1,123 @@
+# Copyright (C) 2017 Sergej Alikov <sergej.alikov@gmail.com>
+
+# This file is part of riemann-fping.
+
+# Automated is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from riemann_fping import fping
+
+
+class ParserTestCase(unittest.TestCase):
+
+    def test_fping_parse_summary(self):
+        output = (
+            '[12:01:02]\n'
+            'host1.example.com : xmt/rcv/%loss = 10/10/0%, min/avg/max = 47.4/53.9/65.3\n'
+            'host2.example.com : xmt/rcv/%loss = 10/10/0%, min/avg/max = 1.0/50.1/100.2\n'
+            '[12:01:12]\n'
+            'host1.example.com : xmt/rcv/%loss = 5/10/50%, min/avg/max = 100.3/150.0/200.4\n'
+            'host2.example.com : xmt/rcv/%loss = 2/10/80%, min/avg/max = 32.9/80.4/130.1\n'
+        )
+
+        obj = fping.Fping(ttl=100)
+
+        data = list(obj.parse(output))
+
+        self.assertEqual(data, [
+            {
+                'service': 'fping/min',
+                'host': 'host1.example.com',
+                'metric': 47.4,
+            },
+            {
+                'service': 'fping/avg',
+                'host': 'host1.example.com',
+                'metric': 53.9,
+            },
+            {
+                'service': 'fping/max',
+                'host': 'host1.example.com',
+                'metric': 65.3,
+            },
+            {
+                'service': 'fping/loss',
+                'host': 'host1.example.com',
+                'metric': 0.0,
+            },
+            {
+                'service': 'fping/min',
+                'host': 'host2.example.com',
+                'metric': 1,
+            },
+            {
+                'service': 'fping/avg',
+                'host': 'host2.example.com',
+                'metric': 50.1,
+            },
+            {
+                'service': 'fping/max',
+                'host': 'host2.example.com',
+                'metric': 100.2,
+            },
+            {
+                'service': 'fping/loss',
+                'host': 'host2.example.com',
+                'metric': 0.0,
+            },
+            {
+                'service': 'fping/min',
+                'host': 'host1.example.com',
+                'metric': 100.3,
+            },
+            {
+                'service': 'fping/avg',
+                'host': 'host1.example.com',
+                'metric': 150.0,
+            },
+            {
+                'service': 'fping/max',
+                'host': 'host1.example.com',
+                'metric': 200.4,
+            },
+            {
+                'service': 'fping/loss',
+                'host': 'host1.example.com',
+                'metric': 50,
+            },
+            {
+                'service': 'fping/min',
+                'host': 'host2.example.com',
+                'metric': 32.9,
+            },
+            {
+                'service': 'fping/avg',
+                'host': 'host2.example.com',
+                'metric': 80.4,
+            },
+            {
+                'service': 'fping/max',
+                'host': 'host2.example.com',
+                'metric': 130.1,
+            },
+            {
+                'service': 'fping/loss',
+                'host': 'host2.example.com',
+                'metric': 80,
+            }])
+
+
+if __name__ == '__main__':
+    unittest.main()
