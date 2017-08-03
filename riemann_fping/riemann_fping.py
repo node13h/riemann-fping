38f73b4832f6954830bdb3a9ee377d750128753f
Sergej Alikov <sergej.alikov@gmail.com>
2017-08-03 22:17:40 +0100

    Implement main application

---
+++ riemann_fping/riemann_fping.py
@@ -0,0 +1,91 @@
+# This file is part of riemann-fping.
+
+# Automated is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import argparse
+
+from riemann_client.transport import TCPTransport, UDPTransport
+from riemann_client.client import Client
+
+from riemann_fping.transport import TLSTransport
+from riemann_fping import fping
+
+
+def parsed_args(args):
+    parser = argparse.ArgumentParser(
+        description='Pings multiple targets and sends results to RIEMANN')
+
+    parser.add_argument(
+        '--host', type=str, default='localhost', help='RIEMANN host')
+    parser.add_argument(
+        '--port', type=int, default=5555, help='RIEMANN port')
+    parser.add_argument(
+        '--protocol', type=str, default='tcp', choices=('tcp', 'udp', 'tls'),
+        help='RIEMANN protocol')
+    parser.add_argument(
+        '--timeout', type=int, default=30,
+        help='Timeout for the TCP connection to RIEMANN')
+    parser.add_argument(
+        '--keyfile', type=str,
+        help='Key file for the TLS connection')
+    parser.add_argument(
+        '--certfile', type=str,
+        help='Certificate file for the TLS connection')
+    parser.add_argument(
+        '--ca-certs', type=str,
+        help='CA certificate file to authenticate the server')
+
+    parser.add_argument(
+        '--fping-cmd', type=str, default='/usr/sbin/fping',
+        help='Path to the fping command')
+    parser.add_argument(
+        '--interval', type=int, default=60,
+        help='Event interval')
+
+    parser.add_argument('target', nargs='+')
+
+    args = parser.parse_args(args)
+
+    if args.protocol == 'tls' and args.ca_certs is None:
+        parser.error('--ca-certs is required for the TLS protocol')
+
+    return args
+
+
+def main():
+    args = parsed_args(sys.argv[1:])
+
+    if args.protocol == 'tcp':
+        transport = TCPTransport(args.host, args.port, args.timeout)
+    elif args.protocol == 'tls':
+        transport = TLSTransport(
+            args.host, args.port, args.timeout,
+            keyfile=args.keyfile, certfile=args.certfile, ca_certs=args.ca_certs)
+    elif args.protocol == 'udp':
+        transport = UDPTransport(args.host, args.port)
+    else:
+        raise RuntimeError('Not supported transport {}'.format(args.transport))
+
+    fp = fping.Fping(args.fping_cmd, interval=args.interval)
+
+    client = Client(transport)
+
+    for summary in fp.ping_summaries(args.target):
+        event = client.create_event(summary)
+
+        with client:
+            client.send_event(event)
+
+    return 0
